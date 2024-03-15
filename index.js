b9911f56ecf6072ea2431e2233f6a40d3e852ec0
Akash (codeguyakash) <codeguyakash@gmail.com>
2024-03-15 18:29:11 +0530

    implement OAuth2-google

--- index.js
+++ index.js
@@ -13,15 +13,70 @@ const OAuth2 = google.auth.OAuth2;
 
 dotenv.config();
 const app = express();
+const numCPUs = os.cpus().length;
+const PORT = process.env.PORT || 5432;
 
 app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const numCPUs = os.cpus().length;
+// ---------------------OAuth2--------
+const myOAuth2Client = new OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  "https://developers.google.com/oauthplayground"
+);
+// const ACCESS_TOKEN = myOAuth2Client.getAccessToken();
+// console.log("first")
 
-const PORT = process.env.PORT || 5432;
+myOAuth2Client.setCredentials({
+  refresh_token: process.env.REFRESH_TOKEN,
+});
+
+const transport = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
+    user: process.env.EMAIL,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN,
+    accessToken: process.env.ACCESS_TOKEN,
+  },
+});
+
+app.post("/sendemail", function (req, res) {
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to: "codeguyakash@techlogs.tech",
+    subject: "Hii OAuth2 from Google-Nodemailer",
+    html: "<p>You have received this email using nodemailer, you are welcome ;)</p>",
+  };
+  console.log(mailOptions);
+  transport.sendMail(mailOptions, function (err, result) {
+    if (err) {
+      console.log(err);
+      res.send({
+        message: err,
+      });
+    } else {
+      console.log(result);
+      transport.close();
+      res.send({
+        message: "Email has been sent: check your inbox!",
+        result,
+      });
+    }
+  });
+});
+
+// ---------------------OAuth2--------
+app.listen(PORT, () => {
+  console.log(`🎉🎉 Server: http://localhost:${PORT}`);
+});
+return;
+
 app.get("/", (_, res) => {
   return res.send({
     numberCPUs: `(${numCPUs})Core CPUs`,
@@ -33,7 +88,7 @@ app.post("/send-emails", upload.single("file"), (req, res) => {
   const { email, password, subject, body } = req.body;
   const file = req?.file;
   const filePath = req?.file?.path;
-  if (emailCount >= 10) return res.send({ response: "limit end" });
+  if (emailCount >= 10) return res.send({ response: "Limit End" });
 
   if (!email && !password && !subject && !body) {
     return res.status(404).send({ message: "Fields Required" });
@@ -43,12 +98,11 @@ app.post("/send-emails", upload.single("file"), (req, res) => {
     return res.status(404).send({ message: "File Required" });
   }
   if (!(file?.mimetype === "text/csv")) {
-    return res.status(404).send({ message: "only CSV File Allowed" });
+    return res.status(404).send({ message: "Only CSV File Allowed" });
   }
   if (!domain) {
     return res.status(404).send({ message: "Only Gmail Allowed" });
   }
-
   // fs.readFile("uploads/clients.csv", "utf8", (err, data) => {
   //   if (err) {
   //     console.error(err);
@@ -106,5 +160,5 @@ app.post("/send-emails", upload.single("file"), (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
+  console.log(`Running - http://localhost:${PORT}`);
 });
