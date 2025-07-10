0e73ca81a543a8ecf742b7090d847ba020e993c7
Akash (codeguyakash) <codeguyakash@gmail.com>
2025-07-10 15:42:51 +0530

    update package.json

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const cluster = require("node:cluster");
-const express = require("express");
-const dotenv = require("dotenv");
-const nodemailer = require("nodemailer");
-const os = require("os");
-const fs = require("fs");
-const cookieParser = require("cookie-parser");
-const csv = require("csv-parser");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const { upload } = require("./middleware/multer");
+const cluster = require('node:cluster');
+const express = require('express');
+const dotenv = require('dotenv');
+const nodemailer = require('nodemailer');
+const os = require('os');
+const fs = require('fs');
+const cookieParser = require('cookie-parser');
+const csv = require('csv-parser');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { upload } = require('./middleware/multer');
 
 dotenv.config();
 const app = express();
@@ -27,31 +27,31 @@ if (cluster.isPrimary) {
   }
 } else {
   const PORT = process.env.PORT || 5432;
-  app.get("/", (_, res) => {
+  app.get('/', (_, res) => {
     return res.send({
       numberCPUs: `(${numCPUs})Core CPUs`,
       server: ` ${process.pid}`,
-      projectName: "Email Broadcast",
+      projectName: 'Email Broadcast',
     });
   });
 
-  app.post("/send-emails", upload.single("file"), (req, res) => {
+  app.post('/send-emails', upload.single('file'), (req, res) => {
     const { email, password, subject, body } = req.body;
     const file = req?.file;
     const filePath = req?.file?.path;
 
     if (!(email && password && subject && body)) {
-      return res.status(404).send({ message: "Fields Required" });
+      return res.status(404).send({ message: 'Fields Required' });
     }
     let domain = email?.match(/@gmail\.com$/);
     if (!filePath) {
-      return res.status(404).send({ message: "File Required" });
+      return res.status(404).send({ message: 'File Required' });
     }
-    if (!(file?.mimetype === "text/csv")) {
-      return res.status(404).send({ message: "only CSV File Allowed" });
+    if (!(file?.mimetype === 'text/csv')) {
+      return res.status(404).send({ message: 'only CSV File Allowed' });
     }
     if (!domain) {
-      return res.status(404).send({ message: "Only Gmail Allowed" });
+      return res.status(404).send({ message: 'Only Gmail Allowed' });
     }
 
     // fs.readFile("uploads/clients.csv", "utf8", (err, data) => {
@@ -69,7 +69,7 @@ if (cluster.isPrimary) {
     // });
     try {
       const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
+        host: 'smtp.gmail.com',
         port: 465,
         secure: true,
         auth: {
@@ -82,9 +82,9 @@ if (cluster.isPrimary) {
         subject: `${subject}`,
         text: `${body}`,
       };
-      fs.createReadStream("uploads/clients.csv")
+      fs.createReadStream('uploads/clients.csv')
         .pipe(csv())
-        .on("data", (row) => {
+        .on('data', (row) => {
           message.to = row.email;
           transporter.sendMail(message, (error, info) => {
             try {
@@ -94,15 +94,15 @@ if (cluster.isPrimary) {
                 console.log(`Email sent: ${info}`);
                 res
                   .status(202)
-                  .send({ message: "Email Sent Success.", info: info });
+                  .send({ message: 'Email Sent Success.', info: info });
               }
             } catch (error) {
               res.status(500).json(error);
             }
           });
         })
-        .on("end", () => {
-          console.log("Send Process Done...");
+        .on('end', () => {
+          console.log('Send Process Done...');
         });
     } catch (error) {
       res.status(500).json(error);
